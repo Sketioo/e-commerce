f27595dec68f73ce42e4ba8d8630e93cace6a332
AmurTigro <martiohusein27@gmail.com>
2023-11-07 14:27:57 +0700

    Modify crud functionality

--- models/product.js
+++ models/product.js
@@ -1,39 +1,83 @@
-const getDb = require('../util/database').getDb;
+const getDb = require("../util/database").getDb;
+const mongodb = require("mongodb");
 
 class Product {
-  constructor(title, price, description, imageUrl) {
+  constructor(title, imageUrl, price, description, id) {
     this.title = title;
     this.price = price;
     this.description = description;
     this.imageUrl = imageUrl;
+    this._id = id;
   }
 
-  save() {
-    const db = getDb();
-    return db
-      .collection('products')
-      .insertOne(this)
-      .then(result => {
-        console.log(result);
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  async save() {
+    try {
+      const db = getDb();
+      let dbOp;
+      if (this._id) {
+        dbOp = await db
+          .collection("products")
+          .updateOne({ _id: new mongodb.ObjectId(this._id) }, { $set: this });
+      } else {
+        dbOp = await db.collection("products").insertOne(this);
+      }
+      return dbOp;
+    } catch (err) {
+      console.log(err);
+    }
   }
 
-  static fetchAll() {
-    const db = getDb();
-    return db
-      .collection('products')
-      .find()
-      .toArray()
-      .then(products => {
-        console.log(products);
-        return products;
-      })
-      .catch(err => {
-        console.log(err);
-      });
+  static async findById(id) {
+    // console.log(id);
+    try {
+      const db = getDb();
+      //* id pada mongodb kita bertipe data object
+      let product = await db
+        .collection("products")
+        .find({ _id: new mongodb.ObjectId(id) })
+        .next();
+      // console.log(product);
+      return product;
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async fetchAll() {
+    try {
+      const db = getDb();
+      const products = await db.collection("products").find().toArray();
+      // console.log(products)
+      return products;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  static async updateOne(id) {
+    try {
+      const db = getDb();
+      console.log(this.title);
+      // await db.collection("products").updateOne({_id: new mongodb.ObjectId(id)},{$set: {
+      //   title: this.title,
+      //   price: this.price,
+      //   description: this.description,
+      //   imageUrl: this.imageUrl
+      // }})
+    } catch (err) {
+      console.log(err);
+    }
+  }
+
+  static async deleteOne(id, product) {
+    try {
+      const db = getDb();
+      await db
+        .collection("products")
+        .deleteOne({ _id: new mongodb.ObjectId(id) });
+    } catch (err) {
+      console.log(err);
+    }
   }
 }
 
--- util/database.js
+++ util/database.js
@@ -1,19 +1,21 @@
-const mongodb = require('mongodb');
+const mongodb = require("mongodb");
 const MongoClient = mongodb.MongoClient;
 
 let _db;
+let mongoConnectionString =
+  "mongodb+srv://tiooluciferr666:8uQ8C4oZx4Uvg2wn@cluster0.4oajlgo.mongodb.net/shop?retryWrites=true";
 
-const mongoConnect = callback => {
-  MongoClient.connect(
-    'mongodb+srv://tiooluciferr666:8uQ8C4oZx4Uvg2wn@cluster0.4oajlgo.mongodb.net/shop?retryWrites=true',
-    {useNewUrlParser: true, useUnifiedTopology: true}
-  )
-    .then(client => {
-      console.log('Connected!');
+const mongoConnect = (callback) => {
+  MongoClient.connect(mongoConnectionString, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+    .then((client) => {
+      console.log("Connected!");
       _db = client.db();
       callback();
     })
-    .catch(err => {
+    .catch((err) => {
       console.log(err);
       throw err;
     });
@@ -23,7 +25,7 @@ const getDb = () => {
   if (_db) {
     return _db;
   }
-  throw 'No database found!';
+  throw "No database found!";
 };
 
 exports.mongoConnect = mongoConnect;
